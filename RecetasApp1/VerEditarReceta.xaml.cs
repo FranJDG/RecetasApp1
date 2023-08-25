dcedf3a30bbc21386839b72fcff6771d69f4fb5e
Fran Diaz <frandiazguitar@gmail.com>
2023-08-25 18:36:55 +0200

    Con opciones de edicion de receta pero no muestra ingredientes ni los edita

--- RecetasApp1/VerEditarReceta.xaml.cs
+++ RecetasApp1/VerEditarReceta.xaml.cs
@@ -1,22 +1,20 @@
 using RecetasApp1.Models;
 using RecetasApp1.Data;
+using System.Text.RegularExpressions;
 
 namespace RecetasApp1;
 
 public partial class VerEditarReceta : ContentPage
 {    
     private Receta receta;
+    private int numeroComensales;
+    private int tiempoCoccion;
 
-	public VerEditarReceta(Receta recetaSeleccionada)
+    public VerEditarReceta(Receta recetaSeleccionada)
 	{
 		InitializeComponent();
 
-		var editButton = new ToolbarItem("Editar", "editar.png", () =>
-		{
-			Editar();
-		});
-
-		ToolbarItems.Add(editButton);
+        EditButton();
 
 		receta = recetaSeleccionada;
         BindingContext = recetaSeleccionada;   //Es necesario para poder hacer el binding en el código xaml  
@@ -25,16 +23,10 @@ public partial class VerEditarReceta : ContentPage
 
 	private void Editar()
 	{
-		ToolbarItems.Clear();
+        SaveButton();		
 
-        SoloLectura(false);		
-
-		var saveButton = new ToolbarItem("Actualizar", "guardar.png", () =>
-		{			
-			Actualizar();
-		});
-		
-		ToolbarItems.Add(saveButton);
+        SoloLectura(false);
+        MostrarMenuEdicion(true);
 	}
 
 	private void Actualizar()
@@ -46,13 +38,22 @@ public partial class VerEditarReceta : ContentPage
                 var db = new SQLiteService().GetConnection();
 
                 receta.Name = nombre.Text;
+                receta.Category = categoria.SelectedItem.ToString();
+                receta.Diners = numeroComensales;
                 receta.Instructions = instrucciones.Text;
+                receta.Time = tiempoCoccion;
 
                 db.Update(receta);               
 
                 SoloLectura(true);
+                MostrarMenuEdicion(false);
 
                 Title = receta.Name.ToUpper();
+                categoriaEntry.Text = receta.Category;
+                comensalesEntry.Text = receta.Diners.ToString();
+                tiempoEntry.Text = receta.Time.ToString();
+
+                EditButton();
 
                 ShowMessage("Se ha actualizado correctamente", 3000);
             }
@@ -90,4 +91,72 @@ public partial class VerEditarReceta : ContentPage
         nombre.IsReadOnly = readable;
         instrucciones.IsReadOnly = readable;
     }
+
+    private void MostrarMenuEdicion(bool edit) 
+    {
+        categoriaLabel.IsVisible = !edit;
+        categoriaEntry.IsVisible = !edit;
+        categoria.IsVisible = edit;
+
+        comensalesLabel.IsVisible = !edit;
+        comensalesEntry.IsVisible = !edit;
+        numComensales.IsVisible = edit;
+        sliderComensales.IsVisible = edit;
+
+        tiempoLabel.IsVisible = !edit;
+        tiempoEntry.IsVisible = !edit;
+        tiempoPreparacion.IsVisible = edit;
+        sliderMinutos.IsVisible = edit;
+    }
+
+    private void slider_ValueChangedUno(object sender, ValueChangedEventArgs e)
+    {
+        // Ajustar el valor del Slider para que sea múltiplo de 1 (sin decimales)
+        int step = 1;
+        int newStep = (int)Math.Round(e.NewValue / step);
+        int personas = newStep * step;
+        ((Slider)sender).Value = personas;
+
+        numeroComensales = personas;
+    }
+
+    private void slider_ValueChangedCinco(object sender, ValueChangedEventArgs e)
+    {
+        // Ajustar el valor del Slider para que sea múltiplo de 5 (sin decimales)
+        int step = 5;
+        int newStep = (int)Math.Round(e.NewValue / step);
+        int minutos = newStep * step;
+        ((Slider)sender).Value = minutos;
+
+        tiempoCoccion = minutos;
+    }
+
+    //***********************************************************************************************
+    //Métodos para cambiar botones del menú
+
+    private void EditButton()
+    {
+        ToolbarItems.Clear();
+
+        var editButton = new ToolbarItem("Editar", "editar.png", () =>
+        {
+            Editar();
+        });
+
+        ToolbarItems.Add(editButton);
+    }
+
+    private void SaveButton()
+    {
+        ToolbarItems.Clear();
+
+        var saveButton = new ToolbarItem("Actualizar", "guardar.png", () =>
+        {
+            Actualizar();
+        });
+
+        ToolbarItems.Add(saveButton);
+    }
+
+    //*************************************************************************************************
 }
